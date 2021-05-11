37fbe098605996f1737093eaf1d9d73d4380b957
borsTiHD <bastian.j@gmx.net>
2021-05-11 09:42:29 +0200

    💄 Linting >> Changed linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,53 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  extends: [
-    '@nuxtjs',
-    'plugin:nuxt/recommended'
-  ],
-  plugins: [
-  ],
-  // add your custom rules here
-  rules: {}
+    root: true,
+    env: {
+        browser: true,
+        node: true
+    },
+    parserOptions: {
+        parser: 'babel-eslint'
+    },
+    extends: ['@nuxtjs', 'plugin:nuxt/recommended'],
+    plugins: [
+    ],
+    // add your custom rules here
+    rules: {
+        indent: ['error', 4, {
+            ignoredNodes: ['TemplateLiteral'],
+            SwitchCase: 1
+        }
+        ],
+        quotes: ['error', 'single'],
+        semi: ['error', 'never'],
+        'comma-dangle': ['error', 'never'],
+        'linebreak-style': ['error', 'windows'],
+        'template-curly-spacing': 'off',
+        'max-len': ['off', { code: 120 }],
+        'no-console': 'off',
+        'arrow-parens': ['error', 'always'],
+        'space-before-function-paren': ['error', 'never'],
+        curly: ['error', 'multi-line'],
+        'import/no-extraneous-dependencies': 'off',
+        'require-await': 0,
+
+        'global-require': 0,
+        'import/no-unresolved': 0,
+        'import/newline-after-import': 0,
+        'no-underscore-dangle': 0,
+
+        'vue/max-attributes-per-line': 'off',
+        'vue/singleline-html-element-content-newline': 'off',
+        'vue/multiline-html-element-content-newline': 'off',
+
+        'vue/html-indent': ['error', 4, {
+            attribute: 1,
+            baseIndent: 1,
+            closeBracket: 0,
+            alignAttributesVertically: true,
+            ignores: []
+        }
+        ],
+
+        'vue/no-v-html': 'off'
+    }
 }
