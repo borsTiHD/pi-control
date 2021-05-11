fddbef2703843ab8937b84dbaba0f0139b246b95
borsTiHD <bastian.j@gmx.net>
2021-05-11 12:47:35 +0200

    ✨ Server API >> Adding endpoints for executing scripts with two 'test' scripts for win/linux.

--- server-middleware/rest-api.js
+++ server-middleware/rest-api.js
@@ -1,5 +1,17 @@
+// Imports
+const path = require('path')
 const bodyParser = require('body-parser')
 const app = require('express')()
+
+// Middleware for handling errors on promise calls
+const asyncHandler = require('../server-middleware/asyncMiddleware')
+
+// ChildProcess Spawn Import
+const ChildProcessClass = require('../classes/ChildProcessClass')
+const childProcessSpawn = new ChildProcessClass()
+
+// ProjectRoot Directory
+const root = path.join(__dirname, '..')
 
 app.use(bodyParser.json())
 
@@ -11,11 +23,38 @@ app.all('/', (req, res) => {
 })
 
 app.all('/help', (req, res) => {
-    console.log('PLEASE HELP ME!')
+    console.log('[API] -> PLEASE HELP ME!')
     res.json({
         _status: 'ok',
         info: 'You are searching for help?'
     })
 })
+
+app.all('/execute', asyncHandler(async(req, res, next) => {
+    const { query } = req
+    const script = query.script
+    const args = query.args || []
+
+    // Promise for spawning
+    const spawn = () => {
+        return new Promise((resolve, reject) => {
+            // Spawn Script
+            childProcessSpawn.execShell(`${root}/scripts/${script}`, args, (pid, output) => { }, (pid, output, exitCode) => {
+                console.log('[API] -> Script finished. Output: ', output)
+                resolve(output)
+            })
+        })
+    }
+
+    // Spawn script
+    const output = await spawn()
+
+    // REST return
+    res.json({
+        _status: 'ok',
+        info: 'Script executed',
+        output
+    })
+}))
 
 module.exports = app
