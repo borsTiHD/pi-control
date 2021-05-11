02250d7bbeffa0b7783e80bae5d32c592b91863d
borsTiHD <bastian.j@gmx.net>
2021-05-11 10:58:22 +0200

    ✨ Pages >> Adding welcome + info and settings page.

---
+++ pages/info.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-row justify="center" align="center">
+        <v-col cols="12" sm="8" md="6">
+            <v-card>
+                <v-card-title class="headline">Pi Info Page</v-card-title>
+                <v-card-text>
+                    <span>Read some infos...</span>
+                </v-card-text>
+            </v-card>
+        </v-col>
+    </v-row>
+</template>
+
+<script>
+export default {
+    data() {
+        return {}
+    }
+}
+</script>
---
+++ pages/settings.vue
@@ -0,0 +1,75 @@
+<template>
+    <div>
+        <v-row justify="center" align="center">
+            <v-col cols="12" sm="8" md="6">
+                <v-card>
+                    <v-card-title class="headline">Test...</v-card-title>
+                    <v-card-text>
+                        <v-row>
+                            <v-col cols="12" md="3">
+                                <v-subheader>test...</v-subheader>
+                            </v-col>
+                            <v-col>
+                                <v-text-field
+                                    v-model="test"
+                                    label="set api token for stock requests"
+                                    hide-details="auto"
+                                    clearable
+                                    single-line
+                                    outlined
+                                />
+                            </v-col>
+                        </v-row>
+                    </v-card-text>
+
+                    <v-divider />
+
+                    <v-card-actions>
+                        <v-spacer />
+                        <v-btn color="primary" text @click="testBtn">test</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-col>
+        </v-row>
+
+        <v-snackbar
+            v-model="snackbar.show"
+            :color="snackbar.color"
+            :elevation="snackbar.elevation"
+        >
+            {{ snackbar.text }}
+            <template #action="{ attrs }">
+                <v-btn
+                    :color="snackbar.btnColor"
+                    text
+                    v-bind="attrs"
+                    @click="snackbar.show = false"
+                >
+                    Close
+                </v-btn>
+            </template>
+        </v-snackbar>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            test: 'Test Text',
+            snackbar: {
+                show: false,
+                text: 'Lorem ipsum dolor sit amet',
+                color: '',
+                btnColor: 'primary',
+                elevation: 24
+            }
+        }
+    },
+    methods: {
+        testBtn() {
+            console.log('pressed...')
+        }
+    }
+}
+</script>
