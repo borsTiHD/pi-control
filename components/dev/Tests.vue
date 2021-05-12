07326df6ee9d819f0eff46cf858c195ab92c6d0c
borsTiHD <bastian.j@gmx.net>
2021-05-12 09:26:08 +0200

    🎨 Dev >> Dev page for testing. Changed folder structure for scripts.

---
+++ components/dev/Tests.vue
@@ -0,0 +1,81 @@
+<template>
+    <v-card>
+        <v-card-title class="headline">Test - Api Calls</v-card-title>
+        <v-card-actions>
+            <v-btn
+                :loading="loadingTestBtn"
+                :disabled="loadingTestBtn"
+                @click="testBtn"
+            >
+                Spawn Test Script
+            </v-btn>
+            <v-btn
+                :loading="loadingHelpBtn"
+                :disabled="loadingHelpBtn"
+                @click="testHelpBtn"
+            >
+                Help Call
+            </v-btn>
+            <v-btn
+                @click="testErrorBtn"
+            >
+                Error Call
+            </v-btn>
+        </v-card-actions>
+    </v-card>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            loadingTestBtn: false,
+            loadingHelpBtn: false
+        }
+    },
+    methods: {
+        testBtn() {
+            const url = '/execute'
+            this.loadingTestBtn = true
+            this.$axios.post(url, null, {
+                params: {
+                    script: 'tests/test',
+                    args: ['a', 'b', 'c']
+                }
+            })
+                .then((res) => {
+                    console.log('Response Data:', res.data)
+                }).catch((error) => {
+                    console.log(error)
+                }).finally(() => {
+                    this.loadingTestBtn = false
+                })
+        },
+        testHelpBtn() {
+            const url = '/help'
+            this.loadingHelpBtn = true
+            this.$axios.get(url)
+                .then((res) => {
+                    console.log('Response Data:', res.data)
+                }).catch((error) => {
+                    console.log(error)
+                }).finally(() => {
+                    this.loadingHelpBtn = false
+                })
+        },
+        testErrorBtn() {
+            const url = '/execute'
+            this.$axios.post(url, null, {
+                params: {
+                    script: 'not-found'
+                }
+            })
+                .then((res) => {
+                    console.log('Response Data:', res.data)
+                }).catch((error) => {
+                    console.log(error)
+                })
+        }
+    }
+}
+</script>
---
+++ pages/dev.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-row justify="center" align="center">
+        <v-col cols="12" sm="8" md="6">
+            <tests-app />
+        </v-col>
+    </v-row>
+</template>
+
+<script>
+import TestsApp from '~/components/dev/Tests.vue'
+
+export default {
+    components: {
+        TestsApp
+    }
+}
+</script>
