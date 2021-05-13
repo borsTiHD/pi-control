4d851b719dc0f6f831bd8e34d12a8bbac4dbfcdf
borsTiHD <bastian.j@gmx.net>
2021-05-13 18:01:49 +0200

    🎨 Info Page >> Preperations for collecting infos.

---
+++ components/infos/Device.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-card>
+        <v-card-title class="headline">Pi Info Page</v-card-title>
+        <v-card-text>
+            <span>Read some infos...</span>
+        </v-card-text>
+    </v-card>
+</template>
+
+<script>
+export default {
+    name: 'Device',
+    data() {
+        return {}
+    }
+}
+</script>
--- pages/info.vue
+++ pages/info.vue
@@ -1,18 +1,30 @@
 <template>
     <v-row justify="center" align="center">
-        <v-col cols="12" sm="8" md="6">
-            <v-card>
-                <v-card-title class="headline">Pi Info Page</v-card-title>
-                <v-card-text>
-                    <span>Read some infos...</span>
-                </v-card-text>
-            </v-card>
+        <v-col cols="12" sm="8" md="6" lg="4">
+            <device />
+        </v-col>
+        <v-col cols="12" sm="8" md="6" lg="4">
+            <device />
+        </v-col>
+        <v-col cols="12" sm="8" md="6" lg="4">
+            <device />
+        </v-col>
+        <v-col cols="12" sm="8" md="6" lg="4">
+            <device />
+        </v-col>
+        <v-col cols="12" sm="8" md="6" lg="4">
+            <device />
         </v-col>
     </v-row>
 </template>
 
 <script>
+import Device from '~/components/infos/Device.vue'
+
 export default {
-    name: 'Info'
+    name: 'Info',
+    components: {
+        Device
+    }
 }
 </script>
