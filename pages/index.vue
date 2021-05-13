267e5222047757290045dc26ec0562bebfef5455
borsTiHD <bastian.j@gmx.net>
2021-05-13 17:40:27 +0200

    💄 Welcome >> Added informations for the app.

--- pages/index.vue
+++ pages/index.vue
@@ -2,42 +2,35 @@
     <v-row justify="center" align="center">
         <v-col cols="12" sm="8" md="6">
             <v-card>
-                <v-card-title class="headline"> Welcome to the Vuetify + Nuxt.js template </v-card-title>
+                <v-card-title class="headline">Welcome to {{ appName }}</v-card-title>
                 <v-card-text>
                     <p>
-                        Vuetify is a progressive Material Design component framework for Vue.js. It was designed to
-                        empower developers to create amazing applications.
+                        {{ appName }} is an App for controlling your raspberry pi.
+                        It was designed to help with information gathering, as well as to simplify control.
                     </p>
                     <p>
-                        For more information on Vuetify, check out the
-                        <a href="https://vuetifyjs.com" target="_blank" rel="noopener noreferrer"> documentation </a>.
+                        For more information on {{ appName }}, check out the
+                        <a :href="repoUrl" target="_blank" rel="noopener noreferrer">github repository</a>.
                     </p>
                     <p>
-                        If you have questions, please join the official
-                        <a href="https://chat.vuetifyjs.com/" target="_blank" rel="noopener noreferrer" title="chat">discord</a>.
-                    </p>
-                    <p>
-                        Find a bug? Report it on the github
+                        Found a bug or have a question? Report it on the github
                         <a
-                            href="https://github.com/vuetifyjs/vuetify/issues"
+                            :href="`${repoUrl}/issues`"
                             target="_blank"
                             rel="noopener noreferrer"
                             title="contribute"
-                        >
-                            issue board </a>.
+                        >issue board</a>.
                     </p>
                     <p>
-                        Thank you for developing with Vuetify and I look forward to bringing more exciting features in
+                        Thank you for trying {{ appName }} and I look forward to bringing more exciting features in
                         the future.
                     </p>
                     <div class="text-xs-right">
-                        <em><small>&mdash; John Leider</small></em>
+                        <em><small>&mdash; {{ appAuthor }}</small></em>
                     </div>
                     <hr class="my-3">
-                    <a href="https://nuxtjs.org/" target="_blank" rel="noopener noreferrer">Nuxt Documentation</a>
-                    <br>
-                    <a href="https://github.com/nuxt/nuxt.js" target="_blank" rel="noopener noreferrer">
-                        Nuxt GitHub
+                    <a :href="repoUrl" target="_blank" rel="noopener noreferrer">
+                        {{ appName }} GitHub
                     </a>
                 </v-card-text>
                 <v-card-actions>
@@ -50,11 +43,24 @@
 </template>
 
 <script>
+import pkg from '~~/package.json'
+
 export default {
     name: 'Index',
     data() {
         return {
             continueRoute: '/info'
+        }
+    },
+    computed: {
+        repoUrl() {
+            return pkg.repository.url
+        },
+        appName() {
+            return pkg.productName
+        },
+        appAuthor() {
+            return pkg.author
         }
     }
 }
