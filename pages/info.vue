a622236d7f89e2656dd4f306d3be21ca0a4714be
borsTiHD <bastian.j@gmx.net>
2021-05-14 21:19:07 +0200

    💄 Info >> Changing component order.

--- pages/info.vue
+++ pages/info.vue
@@ -1,16 +1,16 @@
 <template>
     <v-row justify="center">
         <v-col cols="12" sm="8" md="6" lg="4" class="d-flex flex-column">
-            <system />
-        </v-col>
-        <v-col cols="12" sm="8" md="6" lg="4" class="d-flex flex-column">
-            <device />
+            <uptime />
         </v-col>
         <v-col cols="12" sm="8" md="6" lg="4" class="d-flex flex-column">
             <cpu-info />
         </v-col>
         <v-col cols="12" sm="8" md="6" lg="4" class="d-flex flex-column">
-            <uptime />
+            <system />
+        </v-col>
+        <v-col cols="12" sm="8" md="6" lg="4" class="d-flex flex-column">
+            <device />
         </v-col>
     </v-row>
 </template>
