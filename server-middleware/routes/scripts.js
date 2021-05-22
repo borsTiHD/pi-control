57b33d2706b0646730e1809267e13625b86bb5cb
borsTiHD <bastian.j@gmx.net>
2021-05-22 15:14:49 +0200

    📝 Routes >> Adding route descriptions.

--- server-middleware/routes/scripts.js
+++ server-middleware/routes/scripts.js
@@ -6,15 +6,15 @@ const router = express.Router()
 const controller = require('../controllers/scriptsController')
 
 // Router: '/scripts/..'
-router.all('/', controller.index) /* GET users listing. */
-router.get('/list', controller.list) /* GET users listing. */
-router.post('/execute', controller.execute) /* GET users listing. */
-router.get('/read', controller.read) /* GET users listing. */
-router.get('/download', controller.download) /* GET users listing. */
-router.post('/add/file', controller.addFile) /* GET users listing. */
-router.post('/add/folder', controller.addFolder) /* GET users listing. */
-router.post('/delete', controller.delete) /* GET users listing. */
-router.post('/edit/file', controller.editFile) /* GET users listing. */
-router.post('/edit/folder', controller.editFolder) /* GET users listing. */
+router.all('/', controller.index) /* ALL index. */
+router.get('/list', controller.list) /* GET list of scripts. */
+router.post('/execute', controller.execute) /* POST: executes a script/file. */
+router.get('/read', controller.read) /* GET a file and returns data. */
+router.get('/download', controller.download) /* GET: downloading a file/folder. */
+router.post('/add/file', controller.addFile) /* POST: adding a file to host. */
+router.post('/add/folder', controller.addFolder) /* POST: adding a folder to host. */
+router.post('/delete', controller.delete) /* POST: deleting a given file/folder. */
+router.post('/edit/file', controller.editFile) /* POST: editing a file. */
+router.post('/edit/folder', controller.editFolder) /* POST: editing a folder. */
 
 module.exports = router
