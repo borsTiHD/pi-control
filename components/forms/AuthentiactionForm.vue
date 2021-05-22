bc3767d704e2999f461cb3995d55feb64fabcb1c
borsTiHD <bastian.j@gmx.net>
2021-05-22 17:33:40 +0200

    ✨ Login >> Creating Authentication Form and Login page. No function behind it yet.

---
+++ components/forms/AuthentiactionForm.vue
@@ -0,0 +1,91 @@
+<template>
+    <v-form
+        ref="form"
+        v-model="form.valid"
+        lazy-validation
+        @submit.prevent="onSubmit"
+    >
+        <v-text-field
+            v-model="form.email"
+            :rules="[ emailRules ]"
+            label="E-mail"
+            required
+        />
+
+        <v-text-field
+            v-model="form.password"
+            :counter="20"
+            :rules="[ passwordRules ]"
+            :type="'password'"
+            label="Password"
+            required
+        />
+
+        <v-btn
+            :disabled="btnDisabled"
+            :loading="loading"
+            color="primary"
+            class="mt-2"
+            type="submit"
+        >
+            {{ buttonTitle }}
+        </v-btn>
+    </v-form>
+</template>
+
+<script>
+export default {
+    name: 'AuthenticationForm',
+    props: {
+        buttonTitle: {
+            type: String,
+            required: true,
+            default: ''
+        },
+        loading: {
+            type: Boolean,
+            required: true,
+            default: false
+        }
+    },
+    data: () => ({
+        form: {
+            valid: null,
+            email: '',
+            password: ''
+        }
+    }),
+    computed: {
+        btnDisabled() {
+            // Disable button if parent component is 'loading', or form is 'not valid'
+            if (!this.form.valid || this.loading) return true
+            return false
+        }
+    },
+    mounted() {
+        this.cleaning()
+    },
+    methods: {
+        cleaning() {
+            // Default settings
+            this.form.valid = false
+            this.form.email = ''
+            this.form.password = ''
+        },
+        emailRules(v) {
+            if (!v) return 'E-mail is required'
+            return /.+@.+\..+/.test(v) || 'E-mail must be valid'
+        },
+        passwordRules(v) {
+            if (!v) return 'Password is required'
+            return (v.length >= 4 && v.length <= 20) || 'Password must be at least 4 and less than 20 characters long'
+        },
+        onSubmit() {
+            if (this.$refs.form.validate()) {
+                this.form.finish = true
+                this.$emit('submit', this.form)
+            }
+        }
+    }
+}
+</script>
---
+++ pages/user/login.vue
@@ -0,0 +1,47 @@
+<template>
+    <v-row align="center" justify="center" class="mt-12">
+        <v-col cols="12" md="6" lg="3">
+            <v-card>
+                <v-card-title class="headline">Login</v-card-title>
+                <v-card-text>
+                    <authentication-form button-title="Submit" :loading="loading" @submit="login" />
+                </v-card-text>
+            </v-card>
+        </v-col>
+    </v-row>
+</template>
+
+<script>
+import AuthenticationForm from '@/components/forms/AuthentiactionForm'
+
+export default {
+    name: 'Login',
+    components: {
+        AuthenticationForm
+    },
+    data: () => ({
+        loading: false
+    }),
+    methods: {
+        async login(form) {
+            this.loading = true
+            try {
+                console.log('[App] -> Try to login user:', form)
+                /*
+                const response = await this.$auth.loginWith('local', {
+                    data: {
+                        email: form.email,
+                        password: form.password
+                    }
+                })
+                this.$toast.info(response.data.message)
+                */
+            } catch (error) {
+                console.error('[App] -> Failed to login:', error)
+                this.$toast.error(error.response.data.message)
+            }
+            this.loading = false
+        }
+    }
+}
+</script>
