1b0a489cb69092d58ba234c99199ebab125cc573
borsTiHD <bastian.j@gmx.net>
2021-05-30 21:03:29 +0200

    💄 Login >> Showing user info if already logged in.

--- pages/user/login.vue
+++ pages/user/login.vue
@@ -1,6 +1,21 @@
 <template>
     <v-row align="center" justify="center" class="mt-12">
-        <v-col cols="12" md="6" lg="3">
+        <v-col v-if="$auth.loggedIn" cols="12" md="6" lg="6">
+            <v-card>
+                <v-card-title class="headline">Logged in</v-card-title>
+                <v-card-text>
+                    <v-alert
+                        icon="mdi-shield-lock-outline"
+                        prominent
+                        text
+                        type="info"
+                    >
+                        You are logged in as: {{ $auth.user.email }}
+                    </v-alert>
+                </v-card-text>
+            </v-card>
+        </v-col>
+        <v-col v-else cols="12" md="6" lg="3">
             <v-card>
                 <v-card-title class="headline">Login</v-card-title>
                 <v-card-text>
@@ -34,6 +49,7 @@ export default {
                         password: form.password
                     }
                 })
+                this.$router.push('/dashboard')
             } catch (error) {
                 console.error('[Login] -> Failed to login:', error)
                 this.$toast.error(error.response.data.message)
