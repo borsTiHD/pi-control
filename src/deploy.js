41e398cd676e8ce8936cfe5b4d0ab29620f9a966
borsTiHD <bastian.j@gmx.net>
2021-06-12 16:49:05 +0200

    👷 Deploy >> Changing Script for building, packaging and uploading backend and frontend.

--- src/deploy.js
+++ src/deploy.js
@@ -4,6 +4,7 @@ import fs from 'fs-extra'
 import archiver from 'archiver'
 import webpack from 'webpack'
 import NuxtApp from 'nuxt'
+import minimist from 'minimist'
 import webpackConfig from './../webpack.config.js'
 import nuxtConfig from './client/nuxt.config.js'
 import colors from './color.js'
@@ -16,7 +17,14 @@ const PROJECT_ROOT = path.join(__dirname, '..')
 const DIST_DIR = path.join(PROJECT_ROOT, 'dist')
 const NUXT_DIR = path.join(PROJECT_ROOT, 'src', 'client')
 const BUILD_DIR = path.join(PROJECT_ROOT, 'build')
-const BUILD_FILE = path.join(BUILD_DIR, 'release.zip')
+const SCRIPTS_DIR = path.join(PROJECT_ROOT, 'scripts', 'server')
+const PKG_FILE = path.join(PROJECT_ROOT, 'package.json')
+
+// Package.json
+const pkg = JSON.parse(fs.readFileSync(PKG_FILE)) // import pkg from '../../package.json'
+
+// Arguments
+const argv = minimist(process.argv.slice(2))
 
 // Changing Nuxt Config
 // nuxtConfig.srcDir = NUXT_DIR
@@ -30,26 +38,21 @@ const nuxt = new Nuxt(nuxtConfig)
 // Starting
 init()
 
-// Controlls our process
+// Controlls building
 async function init() {
-    console.log(`${colors.FgCyan}%s${colors.Reset}`, '-'.repeat(30))
-    console.log(`${colors.FgCyan}%s${colors.Reset}`, 'ℹ - BUILDING CLIENT')
-    console.log(`${colors.FgCyan}%s${colors.Reset}`, '-'.repeat(30))
+    logState('(ℹ) BUILDING CLIENT')
     await buildNuxt()
 
-    console.log(`${colors.FgCyan}%s${colors.Reset}`, '-'.repeat(30))
-    console.log(`${colors.FgCyan}%s${colors.Reset}`, 'ℹ - BUILDING BACKEND')
-    console.log(`${colors.FgCyan}%s${colors.Reset}`, '-'.repeat(30))
+    logState('(ℹ) BUILDING BACKEND')
     await buildWebpack()
 
-    console.log(`${colors.FgCyan}%s${colors.Reset}`, '-'.repeat(30))
-    console.log(`${colors.FgCyan}%s${colors.Reset}`, 'ℹ - ARCHIVING APP')
-    console.log(`${colors.FgCyan}%s${colors.Reset}`, '-'.repeat(30))
-    await zipDirectory(DIST_DIR, BUILD_FILE)
+    logState('(ℹ) ARCHIVING APP')
+    await archiveProject()
 
-    console.log(`${colors.FgCyan}%s${colors.Reset}`, '-'.repeat(30))
-    console.log(`${colors.FgCyan}%s${colors.Reset}`, 'ℹ - FINISHED BUILDING APP')
-    console.log(`${colors.FgCyan}%s${colors.Reset}`, '-'.repeat(30))
+    logState('(ℹ) UPLOAD HELPER')
+    await uploadHelper()
+
+    logState('(ℹ) FINISHED BUILDING APP')
     return true
 }
 
@@ -72,46 +75,79 @@ async function buildNuxt() {
         console.error(err)
         return Error(err.message)
     })
+    console.log(`${colors.FgGreen}%s${colors.Reset}`, '(✔) CLIENT FINISHED')
 }
 
 // Building Backend with Webpack
 async function buildWebpack() {
-    /*
-    const compiler = webpack(webpackConfig)
-    compiler.run((err, stats) => { // [Stats Object](#stats-object)
-        // ...
-
-        compiler.close((closeErr) => {
-            // ...
-        })
-    })
-    */
-
     return new Promise((resolve, reject) => {
-        webpack(webpackConfig, (err, stats) => { // [Stats Object](#stats-object)
-            if (err || stats.hasErrors()) {
-                // [Handle errors here](#error-handling)
-                reject(new Error('Webpack Build went wrong!'))
-            }
-            // Done processing
-            resolve()
+        const compiler = webpack(webpackConfig)
+        compiler.run((err, stats) => { // [Stats Object](#stats-object)
+            if (err) { console.error(err) }
+
+            console.log(stats.toString({
+                chunks: false, // Makes the build much quieter
+                colors: true // Shows colors in the console
+            }))
+
+            compiler.close((closeErr) => {
+                if (closeErr) {
+                    console.error(closeErr)
+                    reject(closeErr)
+                }
+                console.log(`${colors.FgGreen}%s${colors.Reset}`, '(✔) BACKEND FINISHED')
+                resolve()
+            })
         })
     })
 }
 
 // Archives our files
-async function zipDirectory(source, out) {
-    const type = 'zip' // Type for packaged file
-    const archive = archiver(type, { zlib: { level: 9 } })
-    const stream = fs.createWriteStream(out)
+async function archiveProject() {
+    // Creates directory, if not exists
+    await fs.mkdir(BUILD_DIR, { recursive: true })
+
+    const type = 'tar' // Type for packaged file
+    const archive = archiver(type, { gzip: true })
+    const file = path.join(BUILD_DIR, `${pkg.name}-v${pkg.version}.tar.gz`)
+    const stream = fs.createWriteStream(file)
 
     return new Promise((resolve, reject) => {
+        // Adding files & folders
         archive
-            .directory(source, false)
-            .on('error', (err) => reject(err))
-            .pipe(stream)
+            .directory(DIST_DIR, 'dist') // Compiled app
+            .directory(SCRIPTS_DIR, path.join('scripts', 'server')) // Server scripts
+            .append(fs.createReadStream(PKG_FILE), { name: 'package.json' }) // Package.json
+            .append(fs.createReadStream(path.join(PROJECT_ROOT, 'ecosystem.json')), { name: 'ecosystem.json' }) // pm2 script
+            .append(fs.createReadStream(path.join(PROJECT_ROOT, 'README.md')), { name: 'README.md' }) // Readme
 
-        stream.on('close', () => resolve())
+        // Archive events
+        archive.on('error', (err) => reject(err))
+        archive.pipe(stream)
+        stream.on('close', () => {
+            console.log(`${colors.FgGreen}%s${colors.Reset}`, '(✔) FILES PACKED')
+            resolve()
+        })
         archive.finalize()
     })
+}
+
+// Uploading to GitHub
+async function uploadHelper() {
+    if ('upload' in argv && argv.upload === 'false') {
+        // If no upload has been specified, don't attempt to upload
+        console.log(`${colors.FgRed}%s${colors.Reset}`, '(⚠) UPLOADING WAS TURNED OFF')
+        return true
+    } else if ('upload' in argv && argv.upload === 'true') {
+        // If no upload has been specified, don't attempt to upload
+        console.log(`${colors.FgGreen}%s${colors.Reset}`, '(✔) UPLOADING BUILD')
+        return true
+    }
+}
+
+// Logging current state to console
+function logState(string) {
+    console.log(`${colors.FgCyan}%s${colors.Reset}`, '-'.repeat(30))
+    console.log(`${colors.FgCyan}%s${colors.Reset}`, string)
+    console.log(`${colors.FgCyan}%s${colors.Reset}`, '-'.repeat(30))
 }
