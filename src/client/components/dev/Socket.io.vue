d5f32b39dd3fdb543cb9292b7a51fd60118dd0a6
borsTiHD <bastian.j@gmx.net>
2021-06-23 15:01:12 +0200

    ✨ Socket.io >> Adding connection and playground on client side.

---
+++ src/client/components/dev/Socket.io.vue
@@ -0,0 +1,89 @@
+<template>
+    <v-card>
+        <v-card-title class="headline">Socket.io Playground</v-card-title>
+        <v-card-actions>
+            <v-btn
+                @click="sendSocketMsg"
+            >
+                Send Message
+            </v-btn>
+            <v-btn
+                @click="requestSocketInterval"
+            >
+                Start Interval
+            </v-btn>
+        </v-card-actions>
+        <v-card-text>
+            <v-textarea
+                name="input-7-1"
+                label="Socket Messages"
+                filled
+                clearable
+                auto-grow
+                readonly
+                :value="text"
+                @click:clear="clearText"
+            />
+        </v-card-text>
+    </v-card>
+</template>
+
+<script>
+import { io } from 'socket.io-client'
+
+export default {
+    name: 'SocketIo',
+    data() {
+        return {
+            dev: false,
+            socket: null,
+            text: ''
+        }
+    },
+    activated() {
+        // Connects to socket.io server
+        this.initSocket()
+    },
+    deactivated() {
+        // Disconnects from socket.io server
+        this.socket.disconnect()
+    },
+    methods: {
+        initSocket() {
+            this.dev = process.env.dev
+
+            // Connecting with socket.io server
+            const socket = this.dev ? io('http://localhost:8800').connect() : io().connect()
+
+            socket.on('connect', () => {
+                console.log('[Socket.io] - Connected to server')
+            })
+            socket.on('message', (message) => {
+                console.log('[Socket.io] - Message from server:', message)
+                this.text += `${message}\n`
+            })
+            socket.on('interval-test', (message) => {
+                console.log('[Socket.io] - Message from server \'interval-test\':', message)
+                this.text += `${message} `
+            })
+            socket.on('diconnect', () => {
+                console.log('[Socket.io] - Disconnected from server')
+            })
+
+            this.socket = socket
+        },
+        sendSocketMsg() {
+            console.log('[Socket.io] - Emit message to server')
+            const randomNumber = Math.floor(Math.random() * 100) + 1
+            this.socket.emit('message', randomNumber)
+        },
+        requestSocketInterval() {
+            console.log('[Socket.io] - Request interval from server')
+            this.socket.emit('interval-test', 2 * 1000)
+        },
+        clearText() {
+            this.text = ''
+        }
+    }
+}
+</script>
