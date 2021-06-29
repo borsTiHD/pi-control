abcda9a0959bfa69ed6f808592d5c65a49c45ce0
borsTiHD <bastian.j@gmx.net>
2021-06-29 18:09:51 +0200

    👽️ Client >> Connecting to socket with 'jwt' token after login.

--- src/client/pages/user/login.vue
+++ src/client/pages/user/login.vue
@@ -57,6 +57,13 @@ export default {
                         password: form.password
                     }
                 })
+
+                // Connecting to Socket.IO server after login with new 'jwt' token
+                this.$socket.io.opts.extraHeaders.Authorization = this.$auth.strategy.token.get()
+                console.log('this.$socket.io', this.$socket.io)
+                this.$socket.open() // Connecting to Socket.IO Server
+
+                // Change route
                 this.$router.push('/dashboard')
             } catch (error) {
                 console.error('[Login] -> Failed to login:', error)
