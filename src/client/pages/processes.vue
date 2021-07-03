23147772298eede7babeb4c38593d8881ae828db
borsTiHD <bastian.j@gmx.net>
2021-07-03 11:08:33 +0200

    💄 Processes >> Adding new empty page with socket room registering.

---
+++ src/client/pages/processes.vue
@@ -0,0 +1,36 @@
+<template>
+    <v-row justify="center">
+        <v-col cols="12" sm="8" md="6" lg="4" class="d-flex flex-column">
+            hello
+        </v-col>
+    </v-row>
+</template>
+
+<script>
+export default {
+    name: 'Processes',
+    components: {},
+    data() {
+        return {}
+    },
+    computed: {
+    },
+    created() {
+    },
+    activated() {
+        // Socket.IO: Joining room
+        this.$socket.emit('room:join', 'processesRoom')
+    },
+    deactivated() {
+        // Socket.IO: Leaving room
+        this.$socket.emit('room:leave', 'processesRoom')
+    },
+    sockets: {
+        processes(message) {
+            console.log('[Socket.io] -> Message from server \'processes\':', message)
+        }
+    },
+    methods: {
+    }
+}
+</script>
