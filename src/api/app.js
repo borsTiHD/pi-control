4f772dcd5503e8f96bf0637a2b822d4e101eb48d
borsTiHD <bastian.j@gmx.net>
2021-07-07 09:24:48 +0200

    🎨 App >> Changing the way, how .env variables are used for setup.

--- src/api/app.js
+++ src/api/app.js
@@ -8,18 +8,20 @@ import initSocketIo from './socket.io/socket.js'
 dotenv.config()
 
 // Env variables with dev default
-process.env.PORT_FRONTEND = process.env.PORT_FRONTEND || 8800 // Default Port: 8800
-process.env.PORT_BACKEND = process.env.PORT_BACKEND || 3001 // Default Port: 3001
+process.env.DEV_PORT_FRONTEND = process.env.DEV_PORT_FRONTEND || 8800 // Default Port: 8800
+process.env.DEV_PORT_BACKEND = process.env.DEV_PORT_BACKEND || 3001 // Default Port: 3001
 
 // Config
 const isDev = process.env.NODE_ENV === 'development'
-const PORT = isDev ? process.env.PORT_BACKEND : process.env.PORT_FRONTEND // On dev we use BACKEND port, on production we use FRONTEND port -> on production we serve front- and backend over the same express server with the same port
+const DEV_HOST_IP = process.env.DEV_HOST_IP || 'localhost'
+const DEV_PORT_FRONTEND = process.env.DEV_PORT_FRONTEND
+const PORT = isDev ? process.env.DEV_PORT_BACKEND : process.env.DEV_PORT_FRONTEND // On dev we use BACKEND port, on production we use FRONTEND port -> on production we serve front- and backend over the same express server with the same port
 if (isDev) console.log('[Server] -> Development:', isDev)
 
 // HTTP Server + Socket.IO Server
 const app = initExpress(isDev) // Express Server
 const httpServer = createServer(app) // HTTP Server serving Express + Socket.IO
-initSocketIo(httpServer, isDev) // Socket.IO Server
+initSocketIo(httpServer, isDev, DEV_HOST_IP, DEV_PORT_FRONTEND) // Socket.IO Server
 
 // Server listening on port
 httpServer.listen(PORT, () => {
--- src/api/socket.io/socket.js
+++ src/api/socket.io/socket.js
@@ -1,15 +1,11 @@
 // Imports
 import passport from 'passport'
 import { Server } from 'socket.io'
-import dotenv from 'dotenv'
 
 // Listeners / Rooms
 import initConnection from './listeners/connection.js'
 import initProcessesRoom from './rooms/processesRoom.js'
 import initTestRoom from './rooms/testRoom.js'
-
-// Loading '.env'
-dotenv.config()
 
 // Socket.io: Register passport as middleware for authentication with jwt
 // url: https://philenius.github.io/web%20development/2021/03/31/use-passportjs-for-authentication-in-socket-io.html
@@ -17,18 +13,14 @@ dotenv.config()
 const wrapMiddlewareForSocketIo = (middleware) => (socket, next) => middleware(socket.request, {}, next)
 
 // Exporting Socket.io
-export default function(httpServer, isDev) {
-    // Application config
-    const HOST_IP = process.env.HOST_IP || 'localhost'
-    const PORT_FRONTEND = process.env.PORT_FRONTEND
-
+export default function(httpServer, isDev, host, port) {
     // Socket.io Options
     // Activating cors on dev environment, so you can connect with different ports
     // Empty config on production -> clients refuse on different origin
     const socketOptions = isDev
         ? {
             cors: {
-                origin: [`http://${HOST_IP}:${PORT_FRONTEND}`, `http://localhost:${PORT_FRONTEND}`],
+                origin: [`http://${host}:${port}`, `http://localhost:${port}`],
                 methods: ['GET', 'POST']
             }
         }
