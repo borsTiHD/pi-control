7bbc14334baf026fd693ef3d507a59e8fedec39c
borsTiHD <bastian.j@gmx.net>
2021-07-07 09:54:16 +0200

    📝 Config >> Documentation for default config.

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export default {
-    // Default config
-    DEV_HOST_IP: 'localhost',
-    DEV_PORT_FRONTEND: 3000,
-    DEV_PORT_BACKEND: 3001,
-    PORT_PRODUCTION: 8800
+    // Default config - can be overwritten with environment variables (.env file)
+    DEV_HOST_IP: 'localhost', // needed for cors setup on developing
+    DEV_PORT_FRONTEND: 3000, // developing port for frontend
+    DEV_PORT_BACKEND: 3001, // developing port for backend
+    PORT_PRODUCTION: 8800 // port for production (used for both - frontend and backend)
 }
