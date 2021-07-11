30804c0f9de9786d62b9889b2f1e2d5687499812
borsTiHD <bastian.j@gmx.net>
2021-07-11 13:31:12 +0200

    🐛 Sidebar >> Headline was accidentally activatable.

--- src/client/components/layout/Sidebar.vue
+++ src/client/components/layout/Sidebar.vue
@@ -7,19 +7,19 @@
         app
     >
         <v-list nav>
+            <v-list-item two-line>
+                <v-list-item-avatar>🚀</v-list-item-avatar>
+                <v-list-item-content>
+                    <v-list-item-title>Application</v-list-item-title>
+                    <v-list-item-subtitle>to the moon</v-list-item-subtitle>
+                </v-list-item-content>
+            </v-list-item>
+
+            <v-divider class="pa-1" />
             <v-list-item-group
                 v-model="selectedItem"
                 color="primary"
             >
-                <v-list-item two-line>
-                    <v-list-item-avatar>🚀</v-list-item-avatar>
-                    <v-list-item-content>
-                        <v-list-item-title>Application</v-list-item-title>
-                        <v-list-item-subtitle>to the moon</v-list-item-subtitle>
-                    </v-list-item-content>
-                </v-list-item>
-
-                <v-divider class="pa-1" />
                 <v-list-item v-for="(item, i) in items" :key="i" :to="item.to" router exact>
                     <v-list-item-action>
                         <v-icon>{{ item.icon }}</v-icon>
