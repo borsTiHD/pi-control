5d1a5f279d729379a4f5c1088a3bc9fd7db3df17
borsTiHD <bastian.j@gmx.net>
2021-07-19 10:16:05 +0200

    💄 Swagger >> Link to swagger docs from dev menu.

---
+++ src/client/components/dev/SwaggerDocs.vue
@@ -0,0 +1,37 @@
+<template>
+    <v-card :elevation="getElevation" :outlined="getOutlined">
+        <v-card-title class="headline">Swagger Api Docs</v-card-title>
+        <v-card-actions>
+            <v-btn @click="swaggerBtn">
+                Go to Swagger Docs
+            </v-btn>
+        </v-card-actions>
+    </v-card>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+    name: 'Swagger',
+    data() {
+        return {
+            loadingTestBtn: false,
+            loadingHelpBtn: false
+        }
+    },
+    computed: {
+        ...mapGetters({
+            getElevation: 'settings/getElevation',
+            getOutlined: 'settings/getOutlined'
+        })
+    },
+    methods: {
+        swaggerBtn() {
+            // const swagger = `http://${this.$config.HOST_IP}:${this.$config.PORT_BACKEND}/api-docs/` // From Dev values
+            const swagger = this.$axios.defaults.baseURL.replace('/api/v1', '/api-docs') // Axios baseURL
+            window.open(swagger, '_blank')
+        }
+    }
+}
+</script>
--- src/client/pages/dev.vue
+++ src/client/pages/dev.vue
@@ -1,6 +1,9 @@
 <template>
     <v-row justify="center" align="center">
         <v-col cols="12" sm="8" md="8">
+            <swagger-docs />
+        </v-col>
+        <v-col cols="12" sm="8" md="8">
             <tests-app />
         </v-col>
         <v-col cols="12" sm="8" md="8">
@@ -10,12 +13,14 @@
 </template>
 
 <script>
+import SwaggerDocs from '~/components/dev/SwaggerDocs.vue'
 import TestsApp from '~/components/dev/Tests.vue'
 import SocketIo from '~/components/dev/Socket.io.vue'
 
 export default {
     name: 'Dev',
     components: {
+        SwaggerDocs,
         TestsApp,
         SocketIo
     }
