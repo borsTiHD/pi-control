581e8e5f71916aecac9f6df842dd17f0cf40d3c2
borsTiHD <bastian.j@gmx.net>
2021-07-20 10:30:26 +0200

    📝 Swagger >> Adding test docs.

--- src/api/express/routes/index.js
+++ src/api/express/routes/index.js
@@ -1,12 +1,42 @@
+/**
+ * @swagger
+ *      components:
+ *          schemas:
+ *              Book:
+ *                  type: object
+ *                  required:
+ *                      - title
+ *                      - author
+ *                      - finished
+ *                  properties:
+ *                      id:
+ *                          type: integer
+ *                          description: The auto-generated id of the book.
+ *                      title:
+ *                          type: string
+ *                          description: The title of your book.
+ *                  example:
+ *                      title: The Pragmatic Programmer
+ */
+
 // Express
 import express from 'express'
+import pkg from '../../package-json.js'
 const router = express.Router()
 
-// Route: '/' -> REST Api index endpoint
+/**
+ * @swagger
+ *  /:
+ *      get:
+ *          description: Welcome to express api!
+ *          responses:
+ *              200:
+ *                  description: Returns a mysterious string.
+ */
 router.all('/', (req, res, next) => {
     res.json({
         _status: 'ok',
-        info: 'Endpoint is set up.'
+        info: `Endpoint is set up. Welcome to express api backend for ${pkg.productName}`
     })
 })
 
