56b7f0b40562db07b66461febc2b00e96b22a410
borsTiHD <bastian.j@gmx.net>
2021-07-29 12:11:25 +0200

    💄 App >> Adding icons for favicon etc.

--- src/client/nuxt.config.js
+++ src/client/nuxt.config.js
@@ -97,7 +97,7 @@ export default {
             { hid: 'description', name: 'description', content: '' }
         ],
         link: [
-            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+            { rel: 'icon', type: 'image/x-icon', href: '/icons/favicon.ico' }
         ]
     },
 
---
+++ src/client/static/icons/source/readme.txt
@@ -0,0 +1,1 @@
+Source: https://emojipedia.org/ninja-cat/
