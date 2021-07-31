18f3b3d82213e845bed3025b6fadd3384538aa9c
borsTiHD <bastian.j@gmx.net>
2021-07-31 15:21:57 +0200

    🐛 Processes >> Fixing item length if a process command has one or more seperations.

--- src/api/socket.io/rooms/processesRoom.js
+++ src/api/socket.io/rooms/processesRoom.js
@@ -43,8 +43,37 @@ export default (io, roomName, interval) => {
                 if (Array.isArray(outputArr) && outputArr.length > 6) {
                     const info = outputArr.slice(0, 5)
                     const columns = outputArr.slice(6, 7)[0].trim().split(/\s+/) // Get only columns, trims leading and trailing whitespaces, also splits at every +whitespace
-                    const processes = outputArr.slice(7).map((item) => {
-                        return item.trim().split(/\s+/) // Trim leading and trailing whitespaces, also splits at every +whitespace
+                    const processes = outputArr.slice(7).map((rawItem) => {
+                        const item = rawItem.trim().split(/\s+/) // Trim leading and trailing whitespaces, also splits at every +whitespace
+                        // Exeption if an item is longer than 12 values
+                        if (Array.isArray(item) && item.length > 12) {
+                            // Problem: If our process item is longer than 12 columns, the last column is a command with separation
+                            //          The solution is to cut out all items with index larger than 11 and turn them into one item
+                            //          e.g.:
+                            //              - oldItem: ["72", "root", "0", "-20", "0", "0", "0", "I", "0,0", "0,0", "0:00.00", "DWC", "Notif+"]
+                            //              - newItem: ["72", "root", "0", "-20", "0", "0", "0", "I", "0,0", "0,0", "0:00.00", "DWC Notif+"]
+                            // Combines every additional index after 11
+                            const combined = item.slice(11).join(' ')
+
+                            // Creating a new array with combined item on index 11
+                            const newItem = []
+                            item.some((value, index) => {
+                                if (index <= 10) {
+                                    // Adding every value with index smaller or equal 10 (includes up to index 10)
+                                    newItem.push(value)
+                                } else if (index === 11) {
+                                    // Index 11 is completely replaced by the 'combined value'
+                                    newItem.push(combined)
+                                }
+                                // Stopping loop if index equal or greater 12 - no need to looping more, its already added with the 'combined value'
+                                return index >= 12
+                            })
+                            // DEBUGGING!!!
+                            console.log('ITEM is greater than 12:', item)
+                            console.log('NEW ITEM:', newItem)
+                            return newItem
+                        }
+                        return item
                     })
 
                     io.to(roomName).emit('processes', { _status: 'ok', data: { info, columns, processes } })
