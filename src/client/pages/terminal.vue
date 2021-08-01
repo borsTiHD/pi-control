c1fa914c6b9a8e493211081e1b296843c7f8ca5f
borsTiHD <bastian.j@gmx.net>
2021-08-01 13:24:26 +0200

    💄 Terminal >> Adding new page. Still empty.

---
+++ src/client/pages/terminal.vue
@@ -0,0 +1,41 @@
+<template>
+    <v-row justify="center">
+        <v-col class="d-flex flex-column">
+            <v-card :elevation="getElevation" :outlined="getOutlined" class="flex d-flex flex-column">
+                <v-card-title class="headline">
+                    <v-icon
+                        large
+                        color="primary"
+                        class="mr-2"
+                    >
+                        mdi-console
+                    </v-icon>
+                    Terminal
+                </v-card-title>
+
+                <v-card-text>
+                    xxx
+                </v-card-text>
+            </v-card>
+        </v-col>
+    </v-row>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+    name: 'Terminal',
+    data() {
+        return {
+            socketRoom: 'terminal'
+        }
+    },
+    computed: {
+        ...mapGetters({
+            getElevation: 'settings/getElevation',
+            getOutlined: 'settings/getOutlined'
+        })
+    }
+}
+</script>
