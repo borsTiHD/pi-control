337a002d3f6b3f0589f9df6ac577c5738ad8d3d2
borsTiHD <bastian.j@gmx.net>
2021-08-01 17:15:54 +0200

    ✨ Terminal >> Adding new components for displaying xterm terminal with events.

---
+++ src/client/components/terminal/VueTerm.vue
@@ -0,0 +1,153 @@
+<template>
+    <div v-resize="onResize" class="xterm" />
+</template>
+
+<script>
+import 'xterm/css/xterm.css'
+import { Terminal } from 'xterm'
+import { FitAddon } from 'xterm-addon-fit'
+import { Unicode11Addon } from 'xterm-addon-unicode11'
+
+export default {
+    name: 'VueTerm',
+    data: () => ({
+        options: {
+            fontFamily: '\'Fira Mono\', monospace',
+            fontSize: 15,
+            cursorBlink: true
+            // rows: 120
+        },
+        resizeActive: false
+    }),
+    mounted() {
+        const term = new Terminal(this.options)
+        this.$fitAddon = new FitAddon()
+        term.loadAddon(this.$fitAddon)
+        term.loadAddon(new Unicode11Addon())
+        term.unicode.activeVersion = '11'
+        term.open(this.$el)
+        this.$fitAddon.fit()
+
+        // Register Terminal
+        this.$terminal = term
+        this.termEvents() // Key+GUI Events
+
+        // Prevents resize event from being executed before mounting
+        this.resizeActive = true
+
+        // Checks if bufferdata exists and writing them in the temrinal
+
+        /*
+        // TODO
+        if (this.$ssh.streamBuffer && this.$ssh.streamBuffer !== '') {
+            this.write(this.$ssh.streamBuffer)
+        }
+        */
+    },
+    beforeDestroy() {
+        /*
+        this.$terminal.selectAll()
+        this.$emit('update:buffer', this.$terminal.getSelection().trim())
+        this.$terminal.clear()
+        */
+    },
+    methods: {
+        fit() {
+            this.$fitAddon.fit()
+        },
+        focus() {
+            this.$terminal.focus()
+        },
+        blur() {
+            this.$terminal.blur()
+        },
+        write(data) {
+            this.$terminal.write(data) // Writes data into the terminal
+            this.$terminal.scrollToBottom()
+        },
+        clear() {
+            this.$terminal.clear()
+        },
+        typing(data) {
+            this.$emit('typing', data) // Sending written data to parent component
+            // this.write(data) // Don't need to write data manually in terminal... they come back themselves via an event
+        },
+        onResize() {
+            if (this.resizeActive) {
+                this.fit()
+            } else {
+                setImmediate(() => {
+                    this.fit()
+                })
+            }
+        },
+        termEvents() {
+            // Keyboard input by the user
+            this.$terminal.onKey((e) => {
+                // Adjusts terminal size
+                this.fit()
+
+                // Key Events -> https://donsnotes.com/tech/charsets/ascii.html
+                if (e.domEvent.key === 'Enter') {
+                    this.typing('\n')
+                } else if (e.domEvent.key === 'Backspace') {
+                    this.typing('\x7F')
+                } else if (e.domEvent.key === 'Escape') {
+                    this.typing('\x1B')
+                } else if (e.domEvent.key === 'Tab') {
+                    this.typing('\x09')
+                } else if (e.domEvent.ctrlKey && e.domEvent.key === 'c') { // STRG+C
+                    this.typing('\x03\n')
+                } else if (e.domEvent.ctrlKey && e.domEvent.key === 'v') { // STRG+V
+                    // Get data from clipboard and paste it in
+                    // const clip = this.$electron.clipboard.readText()
+                    // this.typing(clip)
+                } else if (e.domEvent.key === 'ArrowLeft') {
+                    this.typing('\x1B[D')
+                } else if (e.domEvent.key === 'ArrowRight') {
+                    this.typing('\x1B[C')
+                } else if (e.domEvent.key === 'ArrowUp') {
+                    this.typing('\x1B[A')
+                } else if (e.domEvent.key === 'ArrowDown') {
+                    this.typing('\x1B[B')
+                } else {
+                    // Sends raw key
+                    this.typing(e.domEvent.key)
+                }
+            })
+
+            // Mouse Click Events
+            /*
+            this.$terminal.element.addEventListener('contextmenu', (e) => {
+                // Get data from clipboard and paste it in
+                const clip = this.$electron.clipboard.readText()
+                this.typing(clip)
+            })
+            */
+
+            // Text Selection -> Copy selected text and put it in clipboard
+            /*
+            this.$terminal.onSelectionChange(() => {
+                const selection = this.$terminal.getSelection()
+                this.$electron.clipboard.writeText(selection)
+                this.$toast.info('Info...')
+            })
+            */
+
+            // GUI Events
+            /*
+            this.$terminal.on('blur', () => this.$emit('blur'))
+            this.$terminal.on('focus', () => this.$emit('focus'))
+            this.$terminal.onTitleChange((title) => this.$emit('title-change', title))
+            */
+        }
+    }
+}
+</script>
+
+<style scoped>
+.xterm {
+    height: 100%;
+    width: 100%;
+}
+</style>
--- src/client/pages/terminal.vue
+++ src/client/pages/terminal.vue
@@ -1,7 +1,7 @@
 <template>
     <v-row justify="center">
         <v-col class="d-flex flex-column">
-            <v-card :elevation="getElevation" :outlined="getOutlined" class="flex d-flex flex-column">
+            <v-card id="terminal-card" v-resize="onResize" :elevation="getElevation" :outlined="getOutlined" class="flex d-flex flex-column">
                 <v-card-title class="headline">
                     <v-icon
                         large
@@ -13,8 +13,8 @@
                     Terminal
                 </v-card-title>
 
-                <v-card-text>
-                    xxx
+                <v-card-text :style="`height: ${termHeight}px;`">
+                    <vue-term ref="terminal" @typing="onTyping" />
                 </v-card-text>
             </v-card>
         </v-col>
@@ -22,12 +22,20 @@
 </template>
 
 <script>
+import VueTerm from '@/components/terminal/VueTerm.vue'
+
 import { mapGetters } from 'vuex'
 
 export default {
     name: 'Terminal',
+    components: {
+        VueTerm
+    },
     data() {
         return {
+            interval: null,
+            termHeight: 200,
+            windowHeight: null,
             socketRoom: 'terminal'
         }
     },
@@ -36,6 +44,55 @@ export default {
             getElevation: 'settings/getElevation',
             getOutlined: 'settings/getOutlined'
         })
+    },
+    activated() {
+        // Creates Interval for resizing
+        this.interval = setInterval(() => {
+            if (!this.windowHeight) { this.windowHeight = window.innerHeight } // If there is no saved window size yet, it will be set
+            // If the saved window size is not equal to the current one -> resize
+            if (this.windowHeight !== window.innerHeight) {
+                // Resize Terminal
+                this.onResize()
+                setImmediate(() => {
+                    this.$refs.terminal.fit()
+                })
+                // Saving current height for next check
+                this.windowHeight = window.innerHeight
+            }
+        }, 10)
+    },
+    deactivated() {
+        // Clears interval
+        clearInterval(this.interval)
+        this.interval = null
+    },
+    methods: {
+        onResize() {
+            // Resized div box from the terminal (container)
+            // Calculates the height based on the window height minus the height of the remaining boxes (or the start position of the terminal on the y axis) and additional pixels subtracted for the footer
+            const el = document.getElementById('terminal-card')
+            this.termHeight = window.innerHeight - this.getOffset(el).top - 120
+        },
+        getOffset(el) {
+            /**
+             * getOffset() - Determines the X/Y position of an HTML element
+             *             -> // https://stackoverflow.com/a/28222246
+             * @param   {string}    el  -> HTML Element
+             * @returns {object}        -> Returns X/Y Coordinates in 'px'
+             */
+            const rect = el.getBoundingClientRect()
+            return {
+                left: rect.left + window.scrollX,
+                top: rect.top + window.scrollY,
+                height: rect.height,
+                width: rect.width
+            }
+        },
+        onTyping(data) {
+            // Sends data from terminal to the stream
+            // this.$ssh.stream.write(data)
+            // TODO
+        }
     }
 }
 </script>
