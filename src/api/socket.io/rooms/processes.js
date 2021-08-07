c3b3a835a3e469afc2e60c06661bce4c67914bfa
borsTiHD <bastian.j@gmx.net>
2021-08-07 10:52:20 +0200

    ♻️ Processes >> Refactoring... short break.

--- src/api/socket.io/rooms/processes.js
+++ src/api/socket.io/rooms/processes.js
@@ -2,21 +2,26 @@
 import { spawn } from 'child_process'
 import initListener from '../controllers/roomEventListener.js'
 
+const isWin = process.platform === 'win32'
+const eventName = 'processes'
+
 export default (io, roomName) => {
-    // Childprocess
-    let child = null
+    // Interval
+    let intervalId = null
 
     // Room event listener with callbacks for starting/stopping tasks
     initListener(io, roomName, () => {
         // Create Room Event: Initialize room tasks
-        child = initialize()
+        const duration = 2 * 1000 // Interval duration in milliseconds
+        intervalId = initialize(duration)
     }, () => {
-        // Delete Room Event: Killing child
-        child.kill()
-        child = null
+        // Delete Room Event: Clearing interval
+        clearInterval(intervalId)
+        intervalId = null
     })
 
     // Parsing raw output
+    /*
     async function parseProcessData(raw) {
         const outputArr = raw.split('\n')
         if (Array.isArray(outputArr) && outputArr.length > 6) {
@@ -57,67 +62,73 @@ export default (io, roomName) => {
             throw new Error('Error on parsing script output.')
         }
     }
+    */
+
+    // Collecting processes with 'ps'
+    function unix() {
+        // Collecting chunk data
+        let chunk = ''
+
+        // Spawn command
+        const command = 'ps'
+        const args = ['axo', 'pid,ppid,user,tty,stat,pcpu,pmem,etime,time,comm,args']
+        const child = spawn(command, args)
+
+        // Data output (adding data to chunk)
+        child.stdout.setEncoding('utf8')
+        child.stdout.on('data', (data) => {
+            // Adds output to chunkdata
+            const convertedData = data.toString()
+            chunk += convertedData
+        })
+
+        // Error output
+        child.stderr.setEncoding('utf8')
+        child.stderr.on('data', (data) => {
+            const convertedData = data.toString()
+            io.to(roomName).emit(eventName, { _status: 'error', error: convertedData, info: 'Error output from child process' })
+        })
+
+        // Child closed with error
+        child.on('error', (error) => {
+            io.to(roomName).emit(eventName, { _status: 'error', error: error.message, info: 'Child process closed with error' })
+        })
+
+        // Child closed
+        child.on('close', (code) => {
+            // io.to(roomName).emit(eventName, { _status: 'closed', exitcode: code })
+            io.to(roomName).emit(eventName, { _status: 'ok', data: chunk })
+
+            // Parsing output and send to socket room
+            /*
+            parseProcessData(chunk).then((result) => {
+                io.to(roomName).emit(eventName, { _status: 'ok', data: result })
+            }).catch((err) => {
+                io.to(roomName).emit(eventName, { _status: 'error', error: err.message, info: 'Error on parsing output' })
+            })
+            */
+        })
+    }
+
+    // Collecting processes on windows
+    function isWindows() {
+        io.to(roomName).emit(eventName, { _status: 'error', error: 'Operating system is currently not supported', info: 'Error on starting tasks' })
+    }
 
     // Room logic
-    function initialize() {
+    function initialize(duration) {
         console.log(`[Socket.io] -> Room '${roomName}' starts performing its tasks`)
-        const eventName = 'processes'
         try {
-            // Collecting chunk data
-            let chunkData = null
-
-            // Spawn command
-            const command = 'top'
-            const args = ['-b']
-            const child = spawn(command, args)
-
-            // Data output
-            child.stdout.setEncoding('utf8')
-            child.stdout.on('data', (data) => {
-                const convertedData = data.toString()
-                if (!chunkData) {
-                    // If no chunkdata exists, we will save stream output
-                    chunkData = convertedData
-                } else if (/top - /.test(convertedData)) {
-                    // New data contains a new output/interval
-                    // Old chunkdata was completed
-                    // Parsing old chunkdata and send result to socket room
-                    parseProcessData(chunkData).then((result) => {
-                        io.to(roomName).emit(eventName, { _status: 'ok', data: result })
-                    }).catch((err) => {
-                        io.to(roomName).emit(eventName, { _status: 'error', error: err.message, info: 'Error on parsing output' })
-                    }).finally(() => {
-                        // Old saved data send to socket
-                        // New output will be saved as a new interval of data
-                        // We clean old chunks and overwrite it with new output
-                        // New output might contain line breaks, these will be removed
-                        chunkData = null
-                        chunkData = convertedData.trim()
-                    })
+            const id = setInterval(() => {
+                // Determines data depending on operating system
+                if (isWin) {
+                    isWindows()
                 } else {
-                    // Adds output to chunkdata
-                    chunkData += convertedData
+                    unix()
                 }
-            })
+            }, duration)
 
-            // Error output
-            child.stderr.setEncoding('utf8')
-            child.stderr.on('data', (data) => {
-                const convertedData = data.toString()
-                io.to(roomName).emit(eventName, { _status: 'error', error: convertedData, info: 'Error output from child process' })
-            })
-
-            // Child closed with error
-            child.on('error', (error) => {
-                io.to(roomName).emit(eventName, { _status: 'error', error: error.message, info: 'Child process closed with error' })
-            })
-
-            // Child closed
-            child.on('close', (code) => {
-                io.to(roomName).emit(eventName, { _status: 'closed', exitcode: code })
-            })
-
-            return child
+            return id
         } catch (error) {
             io.to(roomName).emit(eventName, { _status: 'error', error: error.message, info: 'Something went wrong' })
         }
