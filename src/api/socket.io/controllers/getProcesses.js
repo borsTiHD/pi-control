662a4bec5f2d0e557c830e41d992aad8985f668b
borsTiHD <bastian.j@gmx.net>
2021-08-07 12:42:33 +0200

    ♻️ Processes >> Single module for collecting list. Not finished yet.

---
+++ src/api/socket.io/controllers/getProcesses.js
@@ -0,0 +1,114 @@
+import childProcess from 'child_process'
+// import util from 'util'
+
+const TEN_MEGABYTES = 1000 * 1000 * 10
+// const execFile = util.promisify(childProcess.execFile)
+
+const isWin = process.platform === 'win32'
+
+// Collecting processes with 'ps' - copied and modified from https://github.com/sindresorhus/ps-list - thank you
+async function nonWindows(options) {
+    try {
+        return await nonWindowsSingleCall(options)
+    } catch (err) { // If the error is not a parsing error, it should manifest itself in multicall version too.
+        return await nonWindowsMultipleCalls(options)
+    }
+}
+
+// Collecting processes with one 'ps' call
+async function nonWindowsSingleCall(options) {
+    const command = 'ps'
+    const flags = options.all === false ? 'wwxo' : 'awwxo'
+    const psFields = 'pid,ppid,uid,user,tty,stat,%cpu,%mem,etime,time,comm,args' // original: 'pid,ppid,uid,%cpu,%mem,comm,args'
+    const ERROR_MESSAGE_PARSING_FAILED = 'Error on parsing script output'
+
+    // TODO: Use the promise version of `execFile` when https://github.com/nodejs/node/issues/28244 is fixed.
+    const [psPid, stdout] = await new Promise((resolve, reject) => {
+        const child = childProcess.execFile(command, [flags, psFields], { maxBuffer: TEN_MEGABYTES }, (error, stdout) => {
+            if (error === null) {
+                resolve([child.pid, stdout])
+            } else {
+                reject(error)
+            }
+        })
+    })
+
+    // Parsing into Lines
+    const lines = stdout.trim().split('\n')
+    lines.shift() // deletes first line with headers
+
+    // For parsing comm + args
+    let psIndex
+    let commPosition
+    let argsPosition
+
+    // TODO: Use named capture groups when targeting Node.js 10
+    const psOutputRegex = /^[ \t]*(?<pid>\d+)[ \t]+(?<ppid>\d+)[ \t]+(?<uid>\d+)[ \t]+(?<user>\d+)[ \t]+(?<tty>\d+)[ \t]+(?<stat>\d+)[ \t]+(?<cpu>\d+\.\d+)[ \t]+(?<memory>\d+\.\d+)[ \t]+(?<etime>\d+)[ \t]+(?<time>\d+)[ \t]+/
+    // const psOutputRegex = /^[ \t]*(?<pid>\d+)[ \t]+(?<ppid>\d+)[ \t]+(?<uid>\d+)[ \t]+(?<cpu>\d+\.\d+)[ \t]+(?<memory>\d+\.\d+)[ \t]+/
+
+    // Parsing single lines
+    const processes = lines.map((line, index) => {
+        const match = psOutputRegex.exec(line)
+        if (match === null) {
+            throw new Error(ERROR_MESSAGE_PARSING_FAILED)
+        }
+
+        const { pid, ppid, uid, user, tty, stat, cpu, memory, etime, time } = match.groups
+
+        const processInfo = {
+            pid: Number.parseInt(pid, 10),
+            ppid: Number.parseInt(ppid, 10),
+            uid: Number.parseInt(uid, 10),
+            user,
+            tty,
+            stat,
+            cpu: Number.parseFloat(cpu),
+            memory: Number.parseFloat(memory),
+            etime,
+            time,
+            name: undefined,
+            cmd: undefined
+        }
+
+        if (processInfo.pid === psPid) {
+            psIndex = index
+            commPosition = line.indexOf('ps', match[0].length)
+            argsPosition = line.indexOf('ps', commPosition + 2)
+        }
+
+        return processInfo
+    })
+
+    if (psIndex === undefined || commPosition === -1 || argsPosition === -1) {
+        throw new Error(ERROR_MESSAGE_PARSING_FAILED)
+    }
+
+    const commLength = argsPosition - commPosition
+    for (const [index, line] of lines.entries()) {
+        processes[index].name = line.slice(commPosition, commPosition + commLength).trim()
+        processes[index].cmd = line.slice(argsPosition).trim()
+    }
+
+    processes.splice(psIndex, 1)
+    return processes
+}
+
+// Collecting processes with multiple 'ps' calls
+async function nonWindowsMultipleCalls(options) {
+    throw new Error('Multiple Call Method not implemented right now')
+}
+
+// Collecting processes on windows
+function isWindows(options) {
+    throw new Error('Operating system is currently not supported')
+}
+
+// Export module
+export default async(options = {}) => {
+    // Determines collecting data depending on operating system
+    if (isWin) {
+        return isWindows(options)
+    } else {
+        return await nonWindows(options)
+    }
+}
--- src/api/socket.io/rooms/processes.js
+++ src/api/socket.io/rooms/processes.js
@@ -1,8 +1,8 @@
 // Imports
-import { spawn } from 'child_process'
 import initListener from '../controllers/roomEventListener.js'
+import getProcesses from '../controllers/getProcesses.js'
 
-const isWin = process.platform === 'win32'
+// Room Event name
 const eventName = 'processes'
 
 export default (io, roomName) => {
@@ -20,111 +20,16 @@ export default (io, roomName) => {
         intervalId = null
     })
 
-    // Parsing raw output
-    /*
-    async function parseProcessData(raw) {
-        const outputArr = raw.split('\n')
-        if (Array.isArray(outputArr) && outputArr.length > 6) {
-            const info = outputArr.slice(0, 5)
-            const columns = outputArr.slice(6, 7)[0].trim().split(/\s+/) // Get only columns, trims leading and trailing whitespaces, also splits at every +whitespace
-            const processes = outputArr.slice(7).map((rawItem) => {
-                const item = rawItem.trim().split(/\s+/) // Trim leading and trailing whitespaces, also splits at every +whitespace
-                // Exeption if an item is longer than 12 values
-                if (Array.isArray(item) && item.length > 12) {
-                    // Problem: If our process item is longer than 12 columns, the last column is a command with separation
-                    //          The solution is to cut out all items with index larger than 11 and turn them into one item
-                    //          e.g.:
-                    //              - oldItem: ["72", "root", "0", "-20", "0", "0", "0", "I", "0,0", "0,0", "0:00.00", "DWC", "Notif+"]
-                    //              - newItem: ["72", "root", "0", "-20", "0", "0", "0", "I", "0,0", "0,0", "0:00.00", "DWC Notif+"]
-                    // Combines every additional index after 11
-                    const combined = item.slice(11).join(' ')
-
-                    // Creating a new array with combined item on index 11
-                    const newItem = []
-                    item.some((value, index) => {
-                        if (index <= 10) {
-                            // Adding every value with index smaller or equal 10 (includes up to index 10)
-                            newItem.push(value)
-                        } else if (index === 11) {
-                            // Index 11 is completely replaced by the 'combined value'
-                            newItem.push(combined)
-                        }
-                        // Stopping loop if index equal or greater 12 - no need to looping more, its already added with the 'combined value'
-                        return index >= 12
-                    })
-                    return newItem
-                }
-                return item
-            })
-            return { info, columns, processes }
-        } else {
-            console.error('[Socket.io] -> Error on parsing script output:', raw)
-            throw new Error('Error on parsing script output.')
-        }
-    }
-    */
-
-    // Collecting processes with 'ps'
-    function unix() {
-        // Collecting chunk data
-        let chunk = ''
-
-        // Spawn command
-        const command = 'ps'
-        const args = ['axo', 'pid,ppid,user,tty,stat,pcpu,pmem,etime,time,comm,args']
-        const child = spawn(command, args)
-
-        // Data output (adding data to chunk)
-        child.stdout.setEncoding('utf8')
-        child.stdout.on('data', (data) => {
-            // Adds output to chunkdata
-            const convertedData = data.toString()
-            chunk += convertedData
-        })
-
-        // Error output
-        child.stderr.setEncoding('utf8')
-        child.stderr.on('data', (data) => {
-            const convertedData = data.toString()
-            io.to(roomName).emit(eventName, { _status: 'error', error: convertedData, info: 'Error output from child process' })
-        })
-
-        // Child closed with error
-        child.on('error', (error) => {
-            io.to(roomName).emit(eventName, { _status: 'error', error: error.message, info: 'Child process closed with error' })
-        })
-
-        // Child closed
-        child.on('close', (code) => {
-            // io.to(roomName).emit(eventName, { _status: 'closed', exitcode: code })
-            io.to(roomName).emit(eventName, { _status: 'ok', data: chunk })
-
-            // Parsing output and send to socket room
-            /*
-            parseProcessData(chunk).then((result) => {
-                io.to(roomName).emit(eventName, { _status: 'ok', data: result })
-            }).catch((err) => {
-                io.to(roomName).emit(eventName, { _status: 'error', error: err.message, info: 'Error on parsing output' })
-            })
-            */
-        })
-    }
-
-    // Collecting processes on windows
-    function isWindows() {
-        io.to(roomName).emit(eventName, { _status: 'error', error: 'Operating system is currently not supported', info: 'Error on starting tasks' })
-    }
-
     // Room logic
     function initialize(duration) {
         console.log(`[Socket.io] -> Room '${roomName}' starts performing its tasks`)
         try {
-            const id = setInterval(() => {
-                // Determines data depending on operating system
-                if (isWin) {
-                    isWindows()
-                } else {
-                    unix()
+            const id = setInterval(async() => {
+                try {
+                    const psList = await getProcesses()
+                    io.to(roomName).emit(eventName, { _status: 'ok', data: psList })
+                } catch (error) {
+                    io.to(roomName).emit(eventName, { _status: 'error', error: error.message, info: 'Error on starting tasks' })
                 }
             }, duration)
 
