03acb6db71c9ea909250e722a8f278f9ed0ef9cd
borsTiHD <bastian.j@gmx.net>
2021-08-08 15:05:00 +0200

    🎨 Processes >> Faster fetching: First init will collect data before the first interval ends.

--- src/api/socket.io/rooms/processes.js
+++ src/api/socket.io/rooms/processes.js
@@ -5,14 +5,13 @@ import getProcesses from '../controllers/data/getProcesses.js'
 // Room Event name
 const eventName = 'processes'
 
-export default (io, roomName) => {
+export default (io, roomName, duration = 2 * 1000) => {
     // Interval
     let intervalId = null
 
     // Room event listener with callbacks for starting/stopping tasks
     initListener(io, roomName, () => {
         // Create Room Event: Initialize room tasks
-        const duration = 2 * 1000 // Interval duration in milliseconds
         intervalId = initialize(duration)
     }, () => {
         // Delete Room Event: Clearing interval
@@ -23,17 +22,19 @@ export default (io, roomName) => {
     // Room logic
     function initialize(duration) {
         console.log(`[Socket.io] -> Room '${roomName}' starts performing its tasks`)
-        try {
-            const id = setInterval(async() => {
-                try {
-                    const isWin = process.platform === 'win32'
-                    const psList = await getProcesses()
-                    io.to(roomName).emit(eventName, { _status: 'ok', data: { processes: psList, isWin } })
-                } catch (error) {
-                    io.to(roomName).emit(eventName, { _status: 'error', error: error.message, info: 'Error on starting tasks' })
-                }
-            }, duration)
+        async function getData() {
+            try {
+                const isWin = process.platform === 'win32'
+                const psList = await getProcesses()
+                io.to(roomName).emit(eventName, { _status: 'ok', data: { processes: psList, isWin } })
+            } catch (error) {
+                io.to(roomName).emit(eventName, { _status: 'error', error: error.message, info: 'Error on starting tasks' })
+            }
+        }
 
+        try {
+            getData() // Get firsttime, then with interval
+            const id = setInterval(() => { getData() }, duration)
             return id
         } catch (error) {
             io.to(roomName).emit(eventName, { _status: 'error', error: error.message, info: 'Something went wrong' })
