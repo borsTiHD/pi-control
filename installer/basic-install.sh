7de1a41db16f8eaa0bad098b6bb439204a3f3dbb
borsTiHD <bastian.j@gmx.net>
2021-09-25 20:25:52 +0200

    🧪 Installer >> Testing array...

--- installer/basic-install.sh
+++ installer/basic-install.sh
@@ -219,11 +219,14 @@ check_pi_control() {
 
         # Parsing latest release
         local latest_release_json=$(curl -sSL "${URL_LATEST_RELEASE}")
-        local js_parse="JSON.parse(process.argv[1]).assets.map((asset) => { return asset.name }).join(' ')" # Javascript parsing latest_release json and returning asset names as string, seperatet with " "
+        local js_parse="JSON.parse(process.argv[1]).assets.map((asset) => { return asset.name }).join(', ')" # Javascript parsing latest_release json and returning asset names as string, seperatet with ", "
         local assets_string=$(node -pe "${js_parse}" "$(curl -sSL "${URL_LATEST_RELEASE}")")
+        
+        test="test1, test2, test3, test4"
+        IFS=', ' read -r -a assets <<< "$test"
+        
         # eval assets=($assets_string)
-        t="test1 test2 test3 test4"
-        eval assets=($t)
+        # eval assets=($t)
 
         # User select for downloading file
         PS3="Select download file: "
