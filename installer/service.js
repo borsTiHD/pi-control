a3ae330b30f862e6d698241515c78d1ea36b8309
borsTiHD <bastian.j@gmx.net>
2021-09-29 15:50:31 +0200

    🎨 Service >> Adding more tests for installing.

--- installer/service.js
+++ installer/service.js
@@ -1,27 +1,33 @@
 // const Service = require('node-linux').Service
+// const pkg = require('./package.json')
 
+// Getting Argvs
 const argv = process.argv.slice(2)
-console.log(argv)
 
+// Paths
+const PROJECT_ROOT = process.cwd()
+
+// Installing, or deinstalling service
 if (argv[0] === '--install') {
     console.log('INSTALLING')
+    console.log('PROJECT_ROOT:', PROJECT_ROOT)
+
+    /*
+    // Create a new service object
+    const svc = new Service({
+        name: pkg.name,
+        description: pkg.description,
+        script: `${PROJECT_ROOT}`'/path/to/helloworld.js'
+    })
+
+    // Listen for the "install" event, which indicates the
+    // process is available as a service.
+    svc.on('install', function() {
+        svc.start()
+    })
+
+    svc.install()
+    */
 } else if (argv[0] === '--deinstall') {
     console.log('DEINSTALL')
 }
-
-/*
-// Create a new service object
-const svc = new Service({
-    name: 'Hello World',
-    description: 'The nodejs.org example web server.',
-    script: '/path/to/helloworld.js'
-})
-
-// Listen for the "install" event, which indicates the
-// process is available as a service.
-svc.on('install', function() {
-    svc.start()
-})
-
-svc.install()
-*/
