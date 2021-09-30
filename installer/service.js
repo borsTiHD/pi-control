ccaf88ee5d6de83d631e83008194ae104d45ee6d
borsTiHD <bastian.j@gmx.net>
2021-09-30 10:14:23 +0200

    🎨 Service >> Adding service features.

--- installer/service.js
+++ installer/service.js
@@ -1,6 +1,6 @@
 const path = require('path')
-// const Service = require('node-linux').Service
-// const pkg = require('./package.json')
+const Service = require('node-linux').Service
+const pkg = require('./package.json')
 
 // Getting Argvs
 const argv = process.argv.slice(2)
@@ -10,9 +10,6 @@ const PROJECT_ROOT = process.cwd()
 const APP_DIR = path.join(PROJECT_ROOT, 'dist', 'server')
 const APP_FILE = path.join(APP_DIR, 'app.cjs')
 
-console.log('APP_FILE:', APP_FILE)
-
-/*
 // Create a new service object
 const svc = new Service({
     name: pkg.name,
@@ -20,38 +17,48 @@ const svc = new Service({
     script: APP_FILE
 })
 
-// Listen for the "install" event, which indicates the
-// process is available as a service.
-svc.on('install', function() {
-    svc.start()
-})
-
-svc.install()
-*/
-
-// Installing, or deinstalling service
+// Installing, deinstalling, or starting service
 if (argv[0] === '--install') {
-    console.log('INSTALLING')
+    // Installing service
 
-    /*
     // Listen for the "install" event, which indicates the
-    // process is available as a service.
+    // process is available as a service
     svc.on('install', function() {
-        svc.start()
+        console.log('This service is installed.')
+        // svc.start()
     })
 
-    svc.install()
-    */
-} else if (argv[0] === '--deinstall') {
-    console.log('DEINSTALL')
-    /*
-    // Listen for the "uninstall" event so we know when it's done.
-    svc.on('uninstall',function(){
-        console.log('Uninstall complete.');
-        console.log('The service exists: ',svc.exists());
-    });
+    // Just in case this file is run twice.
+    svc.on('alreadyinstalled', function() {
+        console.log('This service is already installed.')
+    })
 
-    // Uninstall the service.
-    svc.uninstall();
-    */
+    // Installing service
+    svc.install()
+} else if (argv[0] === '--deinstall') {
+    // Deinstalling service
+
+    // Listen for the "uninstall" event so we know when it's done
+    svc.on('uninstall', function() {
+        console.log('Uninstall complete.')
+        console.log('The service exists: ', svc.exists())
+    })
+
+    // Listen on error event
+    svc.on('error', function(err) {
+        console.log('ERROR:', err)
+    })
+
+    // Uninstall the service
+    svc.uninstall()
+} else if (argv[0] === '--start') {
+    // Starting service
+
+    // Listen for the "start" event and let us know when the
+    // process has actually started working
+    svc.on('start', function() {
+        console.log(svc.name + ' service started!')
+    })
+
+    svc.start()
 }
